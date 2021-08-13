47fe4f02b86b3771254540feec701a64fdae7313
jsuk10 <jsuk970407@gmail.com>
2021-08-13 21:11:23 +0900

    chore : move flutter folder

---
+++ android/app/src/main/kotlin/com/example/funcoolsex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.funcoolsex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- funcoolsex/android/app/src/main/kotlin/com/example/funcoolsex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.funcoolsex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
