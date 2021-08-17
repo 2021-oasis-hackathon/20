f5c851ababa59eb880f98617ff332a9f646d98a6
jsuk10 <jsuk970407@gmail.com>
2021-08-17 20:05:11 +0900

    feat : Login Scene Finish

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:funcoolsex/firebase.dart';
 import '/mainScene.dart' as mytabs;
+import 'login.dart';
 //import 'package:firebase_database/firebase_database.dart';
 //import 'firebase.dart';
 
 void main() => runApp(
     MaterialApp(
-        home: fireBaseHomePage(title: "test",)
+        home: LoginScreen()
         // home: mytabs.MainScene()
     )
 );
